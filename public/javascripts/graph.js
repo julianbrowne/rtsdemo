1920fe90df5b8bf6bf4a55b19a8c741d3665ead2
Julian Browne <julianbrowne@gmail.com>
2012-04-21 12:56:19 +0100

    initial commit

---
+++ public/javascripts/graph.js
@@ -0,0 +1,53 @@
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+function stream_layers(n, m, o) {
+  if (arguments.length < 3) o = 0;
+  function bump(a) {
+    var x = 1 / (.1 + Math.random()),
+        y = 2 * Math.random() - .5,
+        z = 10 / (.1 + Math.random());
+    for (var i = 0; i < m; i++) {
+      var w = (i / m - y) * z;
+      a[i] += x * Math.exp(-w * w);
+    }
+  }
+  return d3.range(n).map(function() {
+      var a = [], i;
+      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+      for (i = 0; i < 5; i++) bump(a);
+      return a.map(stream_index);
+    });
+}
+
+/* Another layer generator using gamma distributions. */
+function stream_waves(n, m) {
+  return d3.range(n).map(function(i) {
+    return d3.range(m).map(function(j) {
+        var x = 20 * j / m - i / 3;
+        return 2 * x * Math.exp(-.5 * x);
+      }).map(stream_index);
+    });
+}
+
+function stream_index(d, i) {
+  return {x: i, y: Math.max(0, d)};
+}
+
+function transition() {
+  d3.selectAll("path")
+      .data(function() {
+        var d = data1;
+        data1 = data0;
+        return data0 = d;
+      })
+    .transition()
+      .duration(2500)
+      .attr("d", area);
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+
+var http    = require('http');
+var socket  = require('socket.io');
+var mongo   = require('mongodb');
+require('./lib/cursorextension.js').extendCursor(mongo);
+var content = require('node-static');
+var utils   = require('./lib/utils.js');
+
+var dbname   = 'rtsdemo';
+var collname = 'statstore';
+
+// create file handler for local http server
+
+var file = new(content.Server)('./public');
+var handler = function (request, response) {
+    request.addListener('end', function () { file.serve(request, response); });
+}
+
+// start socket server
+
+var socketsApp = http.createServer(handler);
+var io = socket.listen(socketsApp);
+socketsApp.listen(8010);
+
+// create mongo connection and make a db
+
+var mongoServer  = new mongo.Server('localhost', 27017, {});
+
+var dbConnection = new mongo.Db(dbname, mongoServer, {});
+
+// Open db and wait for browser to connect
+
+dbConnection.open(function(error,db){
+
+    var coll = db.collection(collname);
+
+    io.sockets.on('connection', function (socket) {
+        console.log('Received browser connection.');
+        utils.readAndSend(socket, coll);
+    });
+
+});
